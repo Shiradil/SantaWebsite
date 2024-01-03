5443c2465629e1a293ebb104847d728b79f0d4f8
Shiradil <shirbaev04@mail.ru>
2024-01-03 20:23:18 +0600

    Server creating

---
+++ cmd/app.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", HomeHandler).Methods("GET")
+	router.HandleFunc("/letter", LetterHandler).Methods("POST")
+
+	port := ":8080"
+	fmt.Printf("Starting server on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Добро пожаловать на страницу написания письма Санте!")
+}
+
+func LetterHandler(w http.ResponseWriter, r *http.Request) {
+	// тут напишем добавление писем в базу данных
+
+	fmt.Fprintln(w, "Спасибо за ваше письмо! Санта обязательно его увидит.")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module SantaWeb
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.1 // indirect
