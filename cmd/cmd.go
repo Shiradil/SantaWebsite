6499c198be107b268b2b32ebc2cc36f9b9c7e44b
Shiradil <shirbaev04@mail.ru>
2024-01-05 21:18:17 +0600

    Register for child

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -4,6 +4,7 @@ import (
 	"SantaWeb/db"
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"log"
@@ -13,12 +14,24 @@ import (
 )
 
 type Volunteer struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Phone     string
-	Password  string
-	ChildID   int
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	Name     string             `bson:"name"`
+	Surname  string             `bson:"lastName"`
+	Email    string             `bson:"email"`
+	Phone    string             `bson:"phone"`
+	Password string             `bson:"password"`
+	Child    *Child             `bson:"child,omitempty"`
+}
+
+type Child struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Name      string             `bson:"name"`
+	Surname   string             `bson:"surname"`
+	Email     string             `bson:"name"`
+	Phone     string             `bson:"name"`
+	Password  string             `bson:"name"`
+	Wish      string             `bson:"wish"`
+	Volunteer *Volunteer         `bson:"volunteer,omitempty"`
 }
 
 func RunServer() {
@@ -43,6 +56,7 @@ func RunServer() {
 
 	router.HandleFunc("/letter", LetterHandler).Methods("POST")
 	router.HandleFunc("/submit-volunteer-registration", RegisterVolunteerHandler).Methods("POST")
+	router.HandleFunc("/submit-child-registration", RegisterChildHandler).Methods("POST")
 
 	port := ":8080"
 	fmt.Printf("Starting server on port %s...\n", port)
@@ -86,16 +100,54 @@ func RegisterVolunteerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	volunteer := Volunteer{
-		FirstName: firstName,
-		LastName:  lastName,
-		Email:     email,
-		Phone:     phone,
-		Password:  string(hashedPassword),
-		ChildID:   -1,
+		Name:     firstName,
+		Surname:  lastName,
+		Email:    email,
+		Phone:    phone,
+		Password: string(hashedPassword),
+		Child:    nil,
 	}
 
 	collection := db.Client.Database("SantaWeb").Collection("volunteers")
 	_, err = collection.InsertOne(context.Background(), volunteer)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte("Registration Successful!"))
+}
+
+func RegisterChildHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	firstName := r.FormValue("firstName")
+	lastName := r.FormValue("lastName")
+	email := r.FormValue("email")
+	phone := r.FormValue("phone")
+	password := r.FormValue("password")
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	child := Child{
+		Name:      firstName,
+		Surname:   lastName,
+		Email:     email,
+		Phone:     phone,
+		Password:  string(hashedPassword),
+		Wish:      "",
+		Volunteer: nil,
+	}
+
+	collection := db.Client.Database("SantaWeb").Collection("children")
+	_, err = collection.InsertOne(context.Background(), child)
 	if err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
--- db/db.go
+++ db/db.go
@@ -3,8 +3,12 @@ package db
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os/user"
 )
 
 var Client *mongo.Client
@@ -24,7 +28,20 @@ func DbConnection() error {
 	}
 
 	fmt.Println("Connected to MongoDB!")
+	id, err := primitive.ObjectIDFromHex("65980c087ef3cf7bf3fc6870")
 
+	collection := Client.Database("SantaWeb").Collection("volunteers")
+	var result user.User
+	err = collection.FindOne(context.Background(), bson.M{"_id": id}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			fmt.Println("Документ не найден")
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println(result.Username)
 	// Вернуть nil, если соединение успешно
 	return nil
 }
