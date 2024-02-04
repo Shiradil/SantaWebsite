f618a24b3d71705a0143e80e2cf10864ffdbc1be
ernur <220776@astanait.edu.kz>
2024-02-04 20:37:07 +0600

    fix: fixed logfile path

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,11 +13,9 @@ import (
 func main() {
 	log := logrus.New()
 	log.SetFormatter(&logrus.JSONFormatter{})
-	logFile, _ := os.OpenFile("logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, _ := os.OpenFile("cmd/logs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	defer logFile.Close()
 	log.SetOutput(logFile)
-
-	log.Info("hello")
 
 	handlers.InitLogger(log)
 
@@ -31,8 +29,6 @@ func main() {
 
 	port := ":8080"
 
-	log.Error("error")
-	log.Info("hello")
 	log.Infof("Starting server on port %s...\n", port)
 
 	fmt.Printf("Starting server on port %s...\n", port)
