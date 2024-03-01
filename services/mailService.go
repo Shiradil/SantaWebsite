b09a53afc588ddf7fdc594fb4ec11be0f3889b80
ernur <220776@astanait.edu.kz>
2024-03-02 00:55:35 +0600

    feat: add sendMails func

--- services/mailService.go
+++ services/mailService.go
@@ -12,8 +12,7 @@ func SendMail(to, message string) {
 	m := gomail.NewMessage()
 	m.SetHeader("From", from)
 	m.SetHeader("To", to)
-	m.SetHeader("Subject", "Santa web"+
-		"")
+	m.SetHeader("Subject", "Santa web")
 
 	m.SetBody("text/plain", message)
 
@@ -27,5 +26,28 @@ func SendMail(to, message string) {
 
 	if err := d.DialAndSend(m); err != nil {
 		fmt.Println(err)
+	}
+}
+
+func SendMails(receivers []string, message string) {
+	from := "garifullin.ernur@mail.ru"
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", receivers...)
+	m.SetHeader("Subject", "Santa web")
+
+	m.SetBody("text/plain", message)
+
+	d := gomail.NewDialer(
+		"smtp.mail.ru", 465,
+		"garifullin.ernur@mail.ru",
+		"S666jY9t16E9TUh1RUgK",
+	)
+
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	if err := d.DialAndSend(m); err != nil {
+		fmt.Println(err)
+		panic(err)
 	}
 }
